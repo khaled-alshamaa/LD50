ec26d970e9f8ee80dd90051f3523c07410ee5d25
Khaled El-Shamaa <k.el-shamaa@cgiar.org>
2016-06-01 17:05:26 +0300

    a lot of changes ;P
    bla bla bla

--- app.R
+++ app.R
@@ -2,23 +2,25 @@ library(shiny)
 library(ggplot2)
 library(MASS)
 
-LD <- function(Subjects, Mortality, Dose, LD.p=0.5, Link.function='probit') {
-  Dead <- round(Subjects * Mortality, digits=0)
-  Live <- Subjects - Dead
+ld <- function(subjects, responding, dose, ldP=0.5, linkFunction='probit') {
+  dead <- responding
+  live <- subjects - dead
   
   # fitting generalized linear model (error distribution is binomial, and link function is probit)
-  model <- glm(cbind(Dead, Live) ~ Dose, family=binomial(link=Link.function))
+  model <- glm(cbind(dead, live) ~ dose, family=binomial(link=linkFunction))
   
   # predict doses for binomial assay model
-  ld.dose <- dose.p(model, p=c(LD.p))
+  ldDose <- dose.p(model, p=c(ldP))
   
   # return back results
-  x  <- ld.dose[[1]]
-  se <- attr(ld.dose, "SE")[[1]]
-  a  <- model$coefficients[1]
-  b  <- model$coefficients[2]
-
-  list('x'=x, 'se'=se, 'a'=a, 'b'=b)
+  x   <- ldDose[[1]]
+  se  <- attr(ldDose, "SE")[[1]]
+  a   <- summary(model)$coefficients[1,1]
+  b   <- summary(model)$coefficients[2,1]
+  ase <- summary(model)$coefficients[1,2]
+  bse <- summary(model)$coefficients[2,2]
+  
+  list('x'=x, 'se'=se, 'a'=a, 'ase'=ase, 'b'=b, 'bse'=bse)
 }
 
 # Header ------------------------------------------------------------
@@ -28,15 +30,20 @@ header <- headerPanel('LD50')
 # Sidebar -----------------------------------------------------------
 
 sidebar <- sidebarPanel(
+  tags$head(
+    tags$style(type="text/css", ".col-sm-4 { max-width: 350px; } "),
+    tags$style(type="text/css", ".well { max-width: 340px; }")
+  ),
+  
   tags$img(src='icarda.png', height=90, width=190),
   fileInput('datafile', 'Upload Your Data:', accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
   sliderInput('p', 'Effective (or Lethal) Dose:', min=0, max=100, value=50),
-  selectInput('link', 'Link Function:', c('Logit'='logit', 'Probit'='probit', 'log-log'='loglog')),
+  selectInput('link', 'Link Function:', c('Logit'='logit', 'Probit'='probit', 'Complementary log-log'='cloglog')),
   selectInput('transform', 'Take logs of Explanatory:', c('None'='none', 'Base 10'='log', 'Base e'='ln')),
-  numericInput('subjects', 'Number of Subjects:*', 10),
-  uiOutput('mortality.col'),
-  uiOutput('dose.col'),
-  uiOutput('groups.col'),
+  uiOutput('subjectsCol'),
+  uiOutput('respondingCol'),
+  uiOutput('doseCol'),
+  uiOutput('groupsCol'),
   tags$small(tags$em('Tip: click Backspace to unselect groups.'))
 )
 
@@ -45,13 +52,14 @@ sidebar <- sidebarPanel(
 body <- mainPanel(
           tabsetPanel(
             tabPanel('Input Data', tags$h2('Input Data'), dataTableOutput('contents')),
-            tabPanel('Analysis Output', tags$h2('Analysis Output'), tableOutput('ld.list')),
-            tabPanel('Graphics Output', tags$h2('Graphics Output'), downloadButton('downloadGraph', 'Download High Resolution Graph'), plotOutput('ld.graph')),
+            tabPanel('Analysis Output', tags$h2('Analysis Output'), tableOutput('ldList')),
+            tabPanel('Graphics Output', tags$h2('Graphics Output'), downloadButton('downloadGraph', 'Download High Resolution Graph'), plotOutput('ldGraph')),
             tabPanel('Help', tags$h2('Help'), withMathJax(), 
-                     tags$h4('Logit function: $$Pr(x) = \\frac{1}{1+e^{-(a+b.x)}}$$
-                              Probit function: $$Pr (x) = \\phi{(a+b.x)}$$
-                              Normal Cumulative Distribution Function (CDF):
-                              $$\\phi (x) = \\frac{1}{\\sqrt{2\\pi}} \\int_{-\\infty}^{x} e^{(-t^2/2)} dt$$'))
+                     tags$h4('Logit function (1944): $$Pr(x) = \\frac{1}{1+e^{-(a+b.x)}}$$
+                              Probit function (1933): $$Pr(x) = \\phi{(a+b.x)}$$
+                              Where $\\phi$ is the standard normal Cumulative Distribution Function (CDF):
+                              $$\\phi(x) = \\frac{1}{\\sqrt{2\\pi}} \\int_{-\\infty}^{x} e^{(-t^2/2)} dt$$
+                             Complementary log-log (1922): $$Pr(x) = 1-e^{-e^{a+b.x}}$$'))
           )
 )
 
@@ -77,77 +85,101 @@ server <- function(input, output) {
   })
   
   # Build user interface
-  output$mortality.col <- renderUI({
-    selectInput('mortality.col', 'Ratio Responding (e.g. Mortality%):*', as.list(c('', colnames(importedData()))))
+  output$subjectsCol <- renderUI({
+    selectInput('subjectsCol', 'Numbers of Subjects (e.g. Total):*', as.list(c('', colnames(importedData()))))
   })
   
-  output$dose.col <- renderUI({
-    selectInput('dose.col', 'Explanatory Variate (e.g. Dose):*', as.list(c('', colnames(importedData()))))
+  output$respondingCol <- renderUI({
+    selectInput('respondingCol', 'Numbers Responding (e.g. Dead):*', as.list(c('', colnames(importedData()))))
   })
   
-  output$groups.col <- renderUI({
-    selectInput('groups.col', 'Groups (e.g. Treatment):', c('None'='', as.list(colnames(importedData()))))
+  output$doseCol <- renderUI({
+    selectInput('doseCol', 'Explanatory Variate (e.g. Dose):*', as.list(c('', colnames(importedData()))))
+  })
+  
+  output$groupsCol <- renderUI({
+    selectInput('groupsCol', 'Groups (e.g. Treatment):', c('None'='', as.list(colnames(importedData()))))
   })
 
+  # Read the Data
+  modelInputs <- reactive({
+    validate(
+      need(input$subjectsCol != '', 'Please select numbers of subjects column'),
+      need(input$respondingCol != '', 'Please select numbers of responding column'),
+      need(input$doseCol != '', 'Please select explanatory variate column'),
+      need(input$transform == 'none' | all(importedData()[,input$doseCol] > 0), 'You can not take logs for 0 or negative explanatory values!')
+    )
+    
+    subjects   <- importedData()[,input$subjectsCol]
+    responding <- importedData()[,input$respondingCol]
+    
+    if(input$transform == 'none') { 
+      dose <- importedData()[,input$doseCol] 
+    } else if(input$transform == 'log') {
+      dose <- log10(importedData()[,input$doseCol])
+    } else if(input$transform == 'ln') {
+      dose <- log(importedData()[,input$doseCol])
+    }
+    
+    if(input$groupsCol != '') {
+      groups <- as.factor(importedData()[,input$groupsCol])
+    } else {
+      groups <- as.factor(rep.int('All', length(dose)))
+    }
+    
+    list('subjects'=subjects, 'responding'=responding, 'dose'=dose, 'groups'=groups)
+  })
+  
   # Do the Analysis
   results <- reactive({
-    ld.list <- NULL
-    treat <- 'All'
-    
-    if(input$groups.col != '') treat <- levels(importedData()[,input$groups.col])
-    
-    for(i in treat){
-      if(input$groups.col != '') {
-        LD.data <- subset(importedData(), importedData()[[input$groups.col]]==i)
-      }else{
-        LD.data <- importedData()
-      }
+    ldList <- NULL
+
+    for(i in levels(modelInputs()$groups)){
+      subjects   <- modelInputs()$subjects[modelInputs()$groups == i]
+      responding <- modelInputs()$responding[modelInputs()$groups == i]
+      dose       <- modelInputs()$dose[modelInputs()$groups == i]
       
-      mortality <- LD.data[,input$mortality.col]
-      dose <- LD.data[,input$dose.col]
-      
-      val <- LD(input$subjects, mortality, dose, input$p/100, input$link)
-      ld.list <- rbind(ld.list, data.frame(i, input$p, val$x, val$se, val$a, val$b))
+      val <- ld(subjects, responding, dose, input$p/100, input$link)
+      ldList <- rbind(ldList, data.frame(i, input$p, val$x, val$se, val$a, val$ase, val$b, val$bse))
     }
 
-    names(ld.list) <- c('Treat', 'LD', 'Dose', 'SE', 'Intercept (a)', 'Coefficient (b)')
-    row.names(ld.list) <- NULL
-    list(ld.list=ld.list)
+    names(ldList) <- c('Treat', 'LD', 'Dose', 'SE', 'Intercept (a)', 'a SE', 'Coefficient (b)', 'b SE')
+    row.names(ldList) <- NULL
+    ldList
   })
 
-  output$ld.list <- renderTable({
-      results()$ld.list
+  output$ldList <- renderTable({
+    results()
+  }, digits=c(0, 0, 0, 2, 3, 3, 4, 3, 4))
+  
+  graphHeight <- reactive({
+    600*length(levels(results()$Treat))
   })
   
-  graph.height <- reactive({
-    500*length(levels(results()$ld.list$Treat))
-  })
-  
-  mortality.plot <- reactive({
-    g <- ggplot(importedData(), aes(x=importedData()[,input$dose.col], y=importedData()[,input$mortality.col])) + 
+  respondingPlot <- reactive({
+    g <- ggplot(importedData(), aes(x=modelInputs()$dose, y=modelInputs()$responding/modelInputs()$subjects)) + 
       geom_point(position=position_jitter(width=0, height=0.05)) + 
-      #geom_vline(aes(xintercept=Dose, group=Treat), data=results()$ld.list, col='red') + 
-      #geom_text(aes(x=Dose, label=paste0('LD %', input$p), group=Treat), y=0.1, angle=90, vjust=1.5, data=results()$ld.list, col='red') + 
-      xlab(input$dose.col) + ylab(input$mortality.col) + 
-      stat_smooth(method='glm', formula=cbind(y*input$subjects, (1-y)*input$subjects)~x, method.args=list(family=binomial(link=input$link)), fullrange=TRUE, size=1)
+      geom_vline(aes(xintercept=Dose, group=Treat), data=results(), col='red') + 
+      geom_text(aes(x=Dose, label=paste0('LD %', input$p), group=Treat), y=0.1, angle=90, vjust=1.5, data=results(), col='red') + 
+      xlab(input$doseCol) + ylab(paste(input$respondingCol, '%')) + 
+      stat_smooth(method='glm', formula=cbind(y*10, (1-y)*10)~x, method.args=list(family=binomial(link=input$link)), fullrange=TRUE, size=1)
     
-    if(input$groups.col != '') {
-      g <- g + facet_wrap(as.formula(paste('~', input$groups.col)), ncol=1)
-    }
-    
+    if(input$groupsCol != '') g <- g + facet_wrap(as.formula(paste('~', input$groupsCol)), ncol=1)
     g
   })
   
-  output$ld.graph <- renderPlot({
-    print(mortality.plot())
-  }, height=graph.height, res=96)
+  output$ldGraph <- renderPlot({
+    print(respondingPlot())
+  }, height=graphHeight)
   
   
   output$downloadGraph <- downloadHandler(
-    filename = 'test.png',
+    filename = function() { paste0(input$datafile$name, '.png') },
     content = function(file) {
-      png(file, res=600, height=12, width=6, units="in")
-      print(mortality.plot())
+      # 6 inches wide, and manage have golden ratio for graphs! 
+      h <- 6 * length(levels(results()$Treat)) / 1.618
+      png(file, res=600, height=h, width=6, units="in")
+      print(respondingPlot())
       dev.off()
     },
     contentType = 'image/png'
